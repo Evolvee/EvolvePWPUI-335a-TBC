66832bf31bef4edc02972f38dd93220015c9145e
Evolve <karel.vecera@atlas.cz>
2024-09-14 18:51:36 +0200

    Almost production ready™
    jk

---
+++ Interface/AddOns/SharedMedia_MyMedia/MyMedia.lua
@@ -0,0 +1,24 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+LSM:Register("statusbar", "blizzcast", [[Interface\Addons\SharedMedia_MyMedia\statusbar\blizzcast.tga]]) 
+LSM:Register("statusbar", "blizzcastchannel", [[Interface\Addons\SharedMedia_MyMedia\statusbar\blizzcastchannel.tga]]) 
+LSM:Register("statusbar", "blizzcastnonbreakable", [[Interface\Addons\SharedMedia_MyMedia\statusbar\blizzcastnonbreakable.tga]]) 
